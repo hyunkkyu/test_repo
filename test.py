26ae29f02798bcdfdf6d3738a685684347f085f5
hyunkkyu <xuangui95@naver.com>
2024-01-12 17:11:16 +0900

    fix: rename function multiplication_table to muliplication_table2

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-def do_multiplication_tb():
-
+def do_multiplication_tb2():
     for i in range(1, 10):  
-        for j in range(1, 10):  
-            print(f"{i} x {j} = {i*j}")  
-        print("\n")  
+        print(f"{i}단: ", end="")  
+    for j in range(1, 10):  
+        print(f"{i*j}", end=" ")  
+    print("\n")  
 if __name__=='__main__':
- do_multiplication_tb()
+ do_multiplication_tb2()
