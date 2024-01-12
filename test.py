174d5b10a83245b3a75ce3641a167ae5a4fd05dd
hyunkkyu <xuangui95@naver.com>
2024-01-12 17:08:15 +0900

    feat: create mulitiplication table code print 1~9 mulitiplication result

---
+++ test.py
@@ -0,0 +1,8 @@
+def do_multiplication_tb():
+
+    for i in range(1, 10):  
+        for j in range(1, 10):  
+            print(f"{i} x {j} = {i*j}")  
+        print("\n")  
+if __name__=='__main__':
+ do_multiplication_tb()
